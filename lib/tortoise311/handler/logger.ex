2e5590d4bfc7fa2685c5b08daa3c72d320c13bad
Jean-Francois Cloutier <jean.f.cloutier@gmail.com>
2023-07-13 11:25:03 -0400

    Replace Logger.warn with Logger.warning

--- lib/tortoise311/handler/logger.ex
+++ lib/tortoise311/handler/logger.ex
@@ -20,12 +20,12 @@ defmodule Tortoise311.Handler.Logger do
   end
 
   def connection(:down, state) do
-    Logger.warn("Connection has been dropped")
+    Logger.warning("Connection has been dropped")
     {:ok, state}
   end
 
   def connection(:terminating, state) do
-    Logger.warn("Connection is terminating")
+    Logger.warning("Connection is terminating")
     {:ok, state}
   end
 
@@ -35,7 +35,7 @@ defmodule Tortoise311.Handler.Logger do
   end
 
   def subscription({:warn, [requested: req, accepted: qos]}, topic, state) do
-    Logger.warn("Subscribed to #{topic}; requested #{req} but got accepted with QoS #{qos}")
+    Logger.warning("Subscribed to #{topic}; requested #{req} but got accepted with QoS #{qos}")
     {:ok, state}
   end
 
@@ -55,7 +55,7 @@ defmodule Tortoise311.Handler.Logger do
   end
 
   def terminate(reason, _state) do
-    Logger.warn("Client has been terminated with reason: #{inspect(reason)}")
+    Logger.warning("Client has been terminated with reason: #{inspect(reason)}")
     :ok
   end
 end
